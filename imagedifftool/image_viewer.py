1f40a672a4be9e577cf96fcf8d8efc0c8472fd5c
overflowy <overflowy@riseup.net>
2022-11-10 13:30:52 +0100

    image_viewer: Add ImageObj, display ImageObj in ImageViewer

--- imagedifftool/image_viewer.py
+++ imagedifftool/image_viewer.py
@@ -1,4 +1,21 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene, QGraphicsView, QStatusBar, QVBoxLayout, QWidget
+
+
+class ImageObj(QGraphicsView):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.scene = QGraphicsScene(self)
+        self.setScene(self.scene)
+
+        self.item = QGraphicsPixmapItem()
+        self.scene.addItem(self.item)
+
+    def setImage(self, image: str):
+        pass
 
 
 class ImageViewer(QWidget):
@@ -9,6 +26,14 @@ class ImageViewer(QWidget):
 
     def initUI(self):
         self.setLayout(QVBoxLayout())
+        self.imageObj = ImageObj()
+        self.layout().addWidget(self.imageObj)
+
+        self.initStatusBar()
+
+    def initStatusBar(self):
+        self.statusBar = QStatusBar()
+        self.statusBar.showMessage("Ready")
 
     def setImage(self, image: str):
-        pass
+        self.imageObj.setImage(image)
