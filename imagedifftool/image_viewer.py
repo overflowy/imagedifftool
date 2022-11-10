8f0e6bd315a98b1bf01727737b534db567923724
overflowy <overflowy@riseup.net>
2022-11-10 15:28:37 +0100

    image_viewer: Add DropHere

--- imagedifftool/image_viewer.py
+++ imagedifftool/image_viewer.py
@@ -1,4 +1,26 @@
 from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsScene, QGraphicsView, QStatusBar, QVBoxLayout, QWidget
+class DropHere(QLabel):
+    signalFileDropped = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+        self.setText("Drop Here")
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setStyleSheet(
+            """
+            QLabel {
+                border: 2px dashed #aaa;
+                color: #999;
+                font-size: 20px;
+            }
+            """
+        )
+
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, a0: QDragEnterEvent) -> None:
+        return super().dragEnterEvent(a0)
 
 
 class ImageObj(QGraphicsView):
