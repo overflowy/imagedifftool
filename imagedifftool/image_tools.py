5e9a113c39cd65a45ca25113e83627d86be535ab
overflowy <overflowy@riseup.net>
2022-11-12 00:23:22 +0100

    image_tools: Add small fixes

--- imagedifftool/image_tools.py
+++ imagedifftool/image_tools.py
@@ -6,22 +6,22 @@ def getOpenCVImage(filePath: str) -> cv2.Mat:
     return cv2.imread(filePath)
 
 
-def toGrayScale(openCVImg: cv2.Mat) -> cv2.Mat:
-    return cv2.cvtColor(openCVImg, cv2.COLOR_BGR2GRAY)
+def toGrayScale(openCVImage: cv2.Mat) -> cv2.Mat:
+    return cv2.cvtColor(openCVImage, cv2.COLOR_BGR2GRAY)
 
 
-def openCVToQImage(openCVImg: cv2.Mat) -> QImage:
-    return QImage(openCVImg.data, openCVImg.shape[1], openCVImg.shape[0], QImage.Format.Format_BGR888)
+def openCVToQImage(openCVImage: cv2.Mat) -> QImage:
+    return QImage(openCVImage.data, openCVImage.shape[1], openCVImage.shape[0], QImage.Format.Format_BGR888)
 
 
 def _debugShowOpenCVRect(
-    openCVImg: cv2.Mat,
+    openCVImage: cv2.Mat,
     topLeft: tuple[int, int],
     bottomRight: tuple[int, int],
     color: tuple[int, int, int] = (0, 255, 0),
     thickness=2,
 ):
-    cv2.rectangle(openCVImg, topLeft, bottomRight, color, thickness)
-    cv2.imshow("DEBUG", openCVImg)
+    cv2.rectangle(openCVImage, topLeft, bottomRight, color, thickness)
+    cv2.imshow("DEBUG", openCVImage)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
